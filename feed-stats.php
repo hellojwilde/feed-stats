e8447721ffa51a594dfd1ef63c74f3618641d605
speedbreeze <speedbreeze@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-21 03:21:23 +0000

    * feed-stats.php: Edited the description line so that the link to 		the configure page is a relative path rather than one that goes 		to my site.
    git-svn-id: http://svn.wp-plugins.org/feed-stats-plugin/trunk@75164 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feed-stats.php
+++ feed-stats.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: FeedBurner Feed Stats
 	Plugin URI: http://www.speedbreeze.com/2008/02/22/feed-stats-wordpress-plugin/
-	Description: A quick and easy way to view the stats for your FeedBurner feed. After activating the plugin, make sure to  <a href="http://www.speedbreeze.com/wp-admin/options-general.php?page=feed-stats">configure</a> which feed you want to track.
+	Description: A quick and easy way to view the stats for your FeedBurner feed. After activating the plugin, make sure to  <a href="./options-general.php?page=feed-stats">configure</a> which feed you want to track.
 	Version: 1.0.
 	Author: Jonathan Wilde
 	Author URI: http://www.speedbreeze.com
